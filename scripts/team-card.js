46664800c0c9519a7dc1a7be56980b2e6c67cfd0
Akash-cool20 <akash.eduthrill2025@gmail.com>
2023-03-27 23:38:40 +0530

    team section design 2

--- scripts/team-card.js
+++ scripts/team-card.js
@@ -8,13 +8,16 @@ for(parent in cardCollection) {
   let element1 = cardCollection[parent];
   let firstChild = element1.querySelector('.img-container');
   let secondChild = element1.querySelector('.team-details');
+  let thirdchild =  secondChild.querySelector('span')
 
   element1.addEventListener("mouseover", (e) => {
     firstChild.classList.add("myHover-first");
     secondChild.classList.add("myHover");
+    thirdchild.style.display='block';
   })
   element1.addEventListener("mouseout", (e) => {
     firstChild.classList.remove("myHover-first");
     secondChild.classList.remove("myHover");
+    thirdchild.style.display='none';
   })
 }
